2011f4aacd894f8463ade7b2de53d143e3bfd390
Thomas Dall Pedersen <tempo@mail.dk>
2024-12-04 00:06:17 -0200

    metar model created json serializer added
    Implement API integration and update project configuration
    
    - Update .gitignore to exclude appsettings.local.json
    - Modify Program.cs to load configuration, make HTTP request, and handle JSON response
    - Update avwx_metar_service.csproj with necessary package references and include appsettings.local.json
    - Add metar.cs to define Metar and related classes for JSON serialization

--- avwx_metar_service/Program.cs
+++ avwx_metar_service/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using Microsoft.Graph.Models;
+using System.Net.Http.Json;
+using System.Text.Json;
+using avwx_metar_service;
+
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true);
+
+IConfiguration configuration = builder.Build();
+
+string apiPasscode = configuration["ApiPasscode"];
+string apiUrl = configuration["ApiUrl"];
+string responseBody = "";
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        Console.WriteLine("Querying the API...");
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiPasscode}");
+        HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+        response.EnsureSuccessStatusCode(); // Throw if not a success code.
+        responseBody = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine("API Response:");
+        Console.WriteLine(responseBody);
+    }
+    catch (HttpRequestException e)
+    {
+        Console.WriteLine($"Request error: {e.Message}");
+    }
+
+
+    try
+    {
+        var response = JsonSerializer.Deserialize<Dictionary<string, Metar>>(responseBody);
+
+        if (response != null)
+        {
+            foreach (var entry in response)
+            {
+                Console.WriteLine(entry.Value.WindDirection.Value);
+            }
+        }
+    }
+    catch (JsonException e)
+    {
+        Console.WriteLine($"JSON error: {e.Message}");
+    }
+}
+
+
+
---
+++ avwx_metar_service/metar.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace avwx_metar_service
+{
+    public class Metar
+    {
+        [JsonPropertyName("station")]
+        public required string Station { get; set; }
+
+        [JsonPropertyName("time")]
+        public required Time Time { get; set; }
+
+        [JsonPropertyName("wind_direction")]
+        public required WindDirection WindDirection { get; set; }
+
+        [JsonPropertyName("wind_speed")]
+        public required WindSpeed WindSpeed { get; set; }
+
+        [JsonPropertyName("visibility")]
+        public required Visibility Visibility { get; set; }
+
+        [JsonPropertyName("wx_codes")]
+        public required List<WxCode> WxCodes { get; set; }
+
+        [JsonPropertyName("clouds")]
+        public required List<Cloud> Clouds { get; set; }
+
+        [JsonPropertyName("temperature")]
+        public required Temperature Temperature { get; set; }
+
+        [JsonPropertyName("dewpoint")]
+        public required Temperature Dewpoint { get; set; }
+
+        [JsonPropertyName("altimeter")]
+        public required Altimeter Altimeter { get; set; }
+
+    }
+
+    public class Time
+    {
+        [JsonPropertyName("dt")]
+        public required DateTime Dt { get; set; }
+    }
+
+    public class WindDirection
+    {
+        [JsonPropertyName("value")]
+        public required int Value { get; set; }
+    }
+
+    public class WindSpeed
+    {
+        [JsonPropertyName("value")]
+        public required int Value { get; set; }
+    }
+
+    public class Visibility
+    {
+        [JsonPropertyName("value")]
+        public required int Value { get; set; }
+    }
+
+    public class WxCode
+    {
+        [JsonPropertyName("repr")]
+        public required string Value { get; set; }
+    }
+
+    public class Cloud
+    {
+        [JsonPropertyName("type")]
+        public required string Type { get; set; }
+        [JsonPropertyName("altitude")]
+        public required int Altitude { get; set; }
+    }
+
+    public class Temperature
+    {
+        [JsonPropertyName("value")]
+        public required int Value { get; set; }
+    }
+
+    public class Altimeter
+    {
+        [JsonPropertyName("value")]
+        public required int Value { get; set; }
+
+    }
+}
